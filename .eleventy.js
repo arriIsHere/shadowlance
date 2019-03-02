388cd7daed91129fd4edce4001a7cc04f01b8fa8
bblais23 <ben.blais6@gmail.com>
2019-03-02 13:41:28 -0500

    Date filter using moment

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
-module.exports = function(eleventyConfig) {
+const Moment = require('moment');
 
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addFilter('date', function(value, format) {
+    return Moment(value).format(format);
+  });
+  
   return {
     dir: {
       input: 'src',
